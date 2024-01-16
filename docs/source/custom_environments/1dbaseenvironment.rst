1b53616888a2289866b850a5926699b16e988a26
Luke Bhan <luke.bhan@vanderbilt.edu>
2024-01-16 14:24:16 -0800

    add 1d custom envs

--- docs/source/custom_environments/1dbaseenvironment.rst
+++ docs/source/custom_environments/1dbaseenvironment.rst
@@ -1,6 +1,16 @@
 .. _1D-CustomEnvironments:
 
+.. automodule:: pde_control_gym.src.environments1d.base_env_1d
+
 1D Custom Environments
 ======================
 
+Any 1D custom environment can easily be added to the PDE ContRoL Gym. It is best to follow the current environments format to ensure project structure errors are avoided. The following steps are required to add a custom 1D environment. 
 
+1. Build you environment implementing all the required functions by inheriting the base class :class:`PDEEnv1D`. All the details about the base class are given below. 
+
+Base 1D Environment Class
+-------------------------
+
+.. autoclass:: PDEEnv1D
+   :members:
--- pde_control_gym/src/environments1d/base_env_1d.py
+++ pde_control_gym/src/environments1d/base_env_1d.py
@@ -3,39 +3,25 @@ from gymnasium import spaces
 import numpy as np
 import matplotlib.pyplot as plt
 from abc import abstractmethod
+from typing import Type
+from pde_control_gym.src.rewards import BaseReward
 
 class PDEEnv1D(gym.Env):
-    # The entire enviornment and PDE problem gets specified here, so after initialization of the environment each step size is extremely quick
-    # PDE Settings (given as a dictionary for the first argument)
-    #   T - time horizon. Defaults to 1.
-    #   dt - temporal step size. Defaults to 1e-3.
-    #   X - spatial length. Defaults to 1.
-    #   dx - spatial step size. Defaults to 1e-1.
-    #   control_loc:
-    #       control will always be at position 0 in the system. There are two options for control:
-    #           'Neumann': control is applied at du/dx|_{x=0}.
-    #           'Dirchilet': control is applied at x=0.
-    #           Defaults to 'Dirchilet'.
-    #   sensing_loc:
-    #       sensing depends on the control values and takes two parameters in the form of a tuple.
-    #           The first argument can take one of two forms:
-    #               'full': full state measurement is returned at every step.
-    #               'collocated': sensing is given at the point x=0 for Neumann control or its derivative du/dx|_{x=0} for Dirchilet control.
-    #               'opposite': sensing is given at the opposite point x=X.
-    #               Default is 'full'
-    #           The second argument only applies to whether the sensing is Dirchilet or Neumann for the 'opposite'.
-    #               'Neumann': sensing is given for the point at du/dx|_{x=X}
-    #               'Dirchilet': sensing is given for the point at x=X
-    #               Defaults to Dirchilet.
-    #   sensing_noise_func - function to be called as the sample noise. It takes no parameters can can be invoked additively or multiplicatively according to sensing_noise_mode. Defaults to None.
-    #   limit_pde_state_size: Used to end the epsidoe early if a PDE state is above a certain value specified by max_state_value (See below). Defaults to True.
-    #   max_state_value: Only used if limit_pde_state_size is set to True. Sets the maximum value for the PDE at a time step to continue the environment. If over the max value, the episode ends and the reward is given as remaining time steps * max value. Default is 1e10
-    def __init__(self, params):
+    """
+    This is the base env for all 1D PDE problems. All 1D custom environments should inherit this environment and implement the according methods
+
+    :param T: The end time of the simulation.
+    :param dt: The temporal timestep of the simulation.
+    :param X: The spatial length of the simulation.
+    :param dx: The spatial timestep of the simulation.
+    :param reward_class: An instance of the reward class to specify user reward for each simulation step. Must inherit BaseReward class. See `reward documentation <../../utils/rewards.html>`_ for detials.
+    :param normalize: Chooses whether to take action inputs between -1 and 1 and normalize them to betwen (``-max_control_value``, ``max_control_value``) or to leave inputs unaltered. ``max_control_value`` is environment specific so please see the environment for details. 
+    """
+    def __init__(self, T: float, dt: float, X: float, dx: float, reward_class: Type[BaseReward], normalize: bool = False):
         super(PDEEnv1D, self).__init__()
         # Build parameters for number of time steps and number of spatial steps
-        self.parameters = params
-        self.parameters["nt"] = int(round(self.parameters["T"] / self.parameters["dt"]))
-        self.parameters["nx"] = int(round(self.parameters["X"] / self.parameters["dx"]))
+        self.nt = int(round(T/dt))
+        self.nx = int(round(X/dx))
 
       	# Observation Space handled in individual environment due to sensing modifications
 
@@ -43,24 +29,37 @@ class PDEEnv1D(gym.Env):
         self.action_space = spaces.Box(
             np.full(1, -1, dtype="float32"), np.full(1, 1, dtype="float32")
         )
-        if self.parameters["normalize"]:
+        if normalize:
             self.normalize = lambda action, max_value : (action + 1)*max_value - max_value
         else:
             self.normalize = lambda action, max_value : action
         # Holds entire system state. Modified with ghost points for parabolic PDE
-        self.u = np.zeros((self.parameters["nt"], self.parameters["nx"]))
+        self.u = np.zeros((self.nt, self.nx))
         self.time_index = 0
 
         # Setup reward function. Use custom reward with shaping
-        #self.reward = NormReward(self.parameters["nt"],
-        #    self.parameters["reward_norm"], self.parameters["reward_horizon"], self.parameters["max_state_value"], self.parameters["reward_average_length"], self.parameters["truncate_penalty"], self.parameters["terminate_reward"])
-        self.reward = self.parameters["reward_class"](self.parameters["reward_parameters"])
-        
+        self.reward = reward_class
 
     @abstractmethod
-    def step(self, action):
+    def step(self, action: np.ndarray):
+        """
+        step
+
+        Implements the environment behavior for a single timestep depending on a given action
+
+        :param action: The action to take in the environment. 
+        """
         pass
 
     @abstractmethod
-    def reset(self, init_cond, recirculation_func):
+    def reset(self, init_cond: np.ndarray, recirculation_func):
+        """
+        reset 
+
+        Resets the environment at the start of each epsiode
+
+        :param init_cond: The intial condition to reset the PDE :math:`u(x, 0)` to. 
+
+        :param recirculation_func: Specifies the plant parameter function. See each individual environment for details on implementation.
+        """
         pass
