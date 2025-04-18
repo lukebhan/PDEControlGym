4d72ab9793e47190a3ee9cc3ff4138d3b036088a
Gokul G <gokulg232003@gmail.com>
2025-04-18 10:52:26 -0700

    Remove checkpoint files

--- .ipynb_checkpoints/setup-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name="pdecontrolgym",
-        version="0.0.1", 
-        install_requires=["gymnasium", 
-            "numpy", 
-            "matplotlib"], 
-        )
--- pde_control_gym/src/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,5 +0,0 @@
-from pde_control_gym.src.environments1d import TransportPDE1D, ReactionDiffusionPDE1D, TrafficPDE
-from pde_control_gym.src.environments2d import NavierStokes2D
-from pde_control_gym.src.rewards import BaseReward, NormReward, TunedReward1D, NSReward, TrafficARZReward
-
-__all__ = ["TransportPDE1D", "ReactionDiffusionPDE1D", "NavierStokes2D", "BaseReward", "NormReward", "TunedReward1D", "NSReward", "TrafficPDE", "TrafficARZReward"]
