9713087818cdd010d72d897a9aa0ccdef2c174bd
TWintersww <134812478+TWintersww@users.noreply.github.com>
2025-10-08 14:10:08 -0700

    Added basic training loop

--- pde_control_gym/src/environments1d/brain_tumor_env.py
+++ pde_control_gym/src/environments1d/brain_tumor_env.py
@@ -16,16 +16,25 @@ class BrainTumor1D(PDEEnv1D):
                  t1_death_radius: int = 35,
                  reset_init_condition_func = None,
                  total_dosage = None,
+                 verbose = True,
                  **kwargs     
     ):
         super().__init__(**kwargs)
+        self.verbose = verbose
         # Override base env 1d state size
         self.nt = int(round(self.T/self.dt)+1)
         self.nx = int(round(self.X/self.dx)+1)
         self.u = np.zeros((self.nt, self.nx))
         self.xScale = np.linspace(0, self.X, self.nx) # index -> space mapping
-        print(f"nx: {self.nx}, nt: {self.nt}")
-        print(f"u.shape: {self.u.shape}")
+        if self.verbose:
+            print(f"nx: {self.nx}, nt: {self.nt}")
+            print(f"u.shape: {self.u.shape}")
+
+        # Override base env action space to [0, 1]
+        self.action_space = spaces.Box(
+            np.full(1, 0, dtype="float32"),
+            np.full(1, 1, dtype="float32")
+        )
 
         # Simulation parameters
         self.T1_DETECTION_THRESHOLD = 0.8
@@ -75,7 +84,8 @@ class BrainTumor1D(PDEEnv1D):
     
     # Diffusion-Proliferation-Radiation with Action and Reward
     def step(self, control: float):
-        print(f"\tEnvironment: Call step(). Perform dimensionalized finite differencing for time_index={self.time_index+1}")
+        if self.verbose:
+            print(f"\tEnvironment: Call step(). Perform dimensionalized finite differencing for time_index={self.time_index+1}")
 
         if (self.time_index < self.nt-1):
             self.time_index += 1
@@ -99,14 +109,16 @@ class BrainTumor1D(PDEEnv1D):
                 # log after current time_index calculated
                 T1TumorRadius = self.getTumorRadius(self.time_index, 0.8)
                 T2TumorRadius = self.getTumorRadius(self.time_index, 0.16)
-                print(f"\t{self.stage:<15} {self.time_index:<5} {f'{T1TumorRadius:.2f}' if T1TumorRadius is not None else 'None':<15} {f'{T2TumorRadius:.2f}' if T2TumorRadius is not None else 'None':<15}\n")
+                if self.verbose:
+                    print(f"\t{self.stage:<15} {self.time_index:<5} {f'{T1TumorRadius:.2f}' if T1TumorRadius is not None else 'None':<15} {f'{T2TumorRadius:.2f}' if T2TumorRadius is not None else 'None':<15}\n")
 
                 # check if detection radius reached
                 T1TumorRadius = self.getTumorRadius(self.time_index, self.T1_DETECTION_THRESHOLD)
                 self.growthDays = self.time_index
                 if (T1TumorRadius is not None and T1TumorRadius >= self.t1_detection_radius):
                     self.firstTherapyDay = self.time_index + 1
-                    print(f"\n\tGrowth break when time_index={self.growthDays}. Therapy stage begins when time_index={self.firstTherapyDay}. T1TumorRadius={T1TumorRadius:.2f}\n")
+                    if self.verbose:
+                        print(f"\n\tGrowth break when time_index={self.growthDays}. Therapy stage begins when time_index={self.firstTherapyDay}. T1TumorRadius={T1TumorRadius:.2f}\n")
                     self.stage = "Therapy"
                 
                 # check termination conditions
@@ -122,9 +134,11 @@ class BrainTumor1D(PDEEnv1D):
             
             # therapy stage
             if (self.stage == "Therapy"):
-                print(f"\tTherapy: Perform dimensionalized finite differencing for time_index={self.time_index}")
+                if self.verbose:
+                    print(f"\tTherapy: Perform dimensionalized finite differencing for time_index={self.time_index}")
                 applied_dosage = control * self.remaining_dosage
-                print(f"\tAction = {control}. Remaining dosage = {self.remaining_dosage}. Dosage for current timestep = {applied_dosage}")
+                if self.verbose:
+                    print(f"\tAction = {control}. Remaining dosage = {self.remaining_dosage}. Dosage for current timestep = {applied_dosage}")
                 self.remaining_dosage -= applied_dosage
 
                 dArray = np.zeros_like(self.u[0])
@@ -152,13 +166,15 @@ class BrainTumor1D(PDEEnv1D):
                 # log after current time_index calculated
                 T1TumorRadius = self.getTumorRadius(self.time_index, 0.8)
                 T2TumorRadius = self.getTumorRadius(self.time_index, 0.16)
-                print(f"\t{self.stage:<15} {self.time_index:<5} {f'{T1TumorRadius:.2f}' if T1TumorRadius is not None else 'None':<15} {f'{T2TumorRadius:.2f}' if T2TumorRadius is not None else 'None':<15}")
+                if self.verbose:
+                    print(f"\t{self.stage:<15} {self.time_index:<5} {f'{T1TumorRadius:.2f}' if T1TumorRadius is not None else 'None':<15} {f'{T2TumorRadius:.2f}' if T2TumorRadius is not None else 'None':<15}")
 
                 # check if therapy completed: if self.remaining_dosage < 1
                 if self.remaining_dosage < 1:
                     self.therapyDays = self.time_index - self.growthDays
                     self.firstPostTherapyDay = self.time_index + 1
-                    print(f"\n\tTherapy completed. Begin Post-Therapy starting time_index={self.firstPostTherapyDay}\n")
+                    if self.verbose:
+                        print(f"\n\tTherapy completed. Begin Post-Therapy starting time_index={self.firstPostTherapyDay}\n")
                     self.stage = "Post-Therapy"
 
                 # check termination conditions
@@ -166,7 +182,7 @@ class BrainTumor1D(PDEEnv1D):
                 truncate = self.truncate()
                 return (
                   self.u[self.time_index],
-                  self.reward_class.reward(uVec=self.u, time_index=self.time_index, terminate=terminate, truncate=truncate, action=control, t_benchmark = self.t_benchmark, tumor_radius=T1TumorRadius, treatment_radius=treatmentRadius, applied_dosage=applied_dosage), 
+                  self.reward_class.reward(uVec=self.u, time_index=self.time_index, terminate=terminate, truncate=truncate, action=control, verbose=self.verbose, t_benchmark = self.t_benchmark, tumor_radius=T1TumorRadius, treatment_radius=treatmentRadius, applied_dosage=applied_dosage), 
                   terminate,
                   truncate,
                   {"stage": self.stage},
@@ -174,7 +190,8 @@ class BrainTumor1D(PDEEnv1D):
             
             # post-therapy stage
             if (self.stage == "Post-Therapy"):
-                print(f"\tPost-Therapy: Perform dimensionalized finite differencing for time_index={self.time_index}")
+                if self.verbose:
+                    print(f"\tPost-Therapy: Perform dimensionalized finite differencing for time_index={self.time_index}")
 
                 # perform finite differencing
                 nextU[1:self.nx-1] = ( currU[1:self.nx-1] +
@@ -191,18 +208,29 @@ class BrainTumor1D(PDEEnv1D):
                 # log after current time_index calculated
                 T1TumorRadius = self.getTumorRadius(self.time_index, 0.8)
                 T2TumorRadius = self.getTumorRadius(self.time_index, 0.16)
-                print(f"{self.stage:<15} {self.time_index:<5} {f'{T1TumorRadius:.2f}' if T1TumorRadius is not None else 'None':<15} {f'{T2TumorRadius:.2f}' if T2TumorRadius is not None else 'None':<15}\n")
+                if self.verbose:
+                    print(f"{self.stage:<15} {self.time_index:<5} {f'{T1TumorRadius:.2f}' if T1TumorRadius is not None else 'None':<15} {f'{T2TumorRadius:.2f}' if T2TumorRadius is not None else 'None':<15}\n")
 
                 # check termination conditions
                 terminate = self.terminate()
                 truncate = self.truncate()
-                return (
-                  self.u[self.time_index],
-                  self.reward_class.reward(uVec=self.u, time_index=self.time_index, terminate=terminate, truncate=truncate, action=control, t_benchmark = self.t_benchmark),
-                  terminate,
-                  truncate,
-                  {"stage": self.stage},
-                )
+
+                if (terminate or truncate):
+                    return (
+                      self.u[self.time_index],
+                      self.reward_class.reward(uVec=self.u, time_index=self.time_index, terminate=terminate, truncate=truncate, action=control, verbose=self.verbose, t_benchmark = self.t_benchmark),
+                      terminate,
+                      truncate,
+                      {"stage": self.stage},
+                    )
+                else:
+                    return (
+                      self.u[self.time_index],
+                      0, #no reward if not terminate or truncate during post-therapy
+                      terminate,
+                      truncate,
+                      {"stage": self.stage},
+                    )
 
 
         
@@ -214,14 +242,15 @@ class BrainTumor1D(PDEEnv1D):
             if (self.stage == "Post-Therapy"):
                 self.postTherapyDays = self.time_index - self.therapyDays - self.growthDays
                 self.simulationDays = self.growthDays + self.therapyDays + self.postTherapyDays
-            print(f"\nTerminate: self.time_index is at or exceeds {self.nt - 1}\n")
-            print(f"self.simulationDays {self.simulationDays}")
-            print(f"self.growthDays {self.growthDays}")
-            print(f"self.therapyDays {self.therapyDays}")
-            print(f"self.postTherapyDays {self.postTherapyDays}")
-            print(f"self.firstTherapyDay {self.firstTherapyDay}")
-            print(f"self.firstPostTherapyDay {self.firstPostTherapyDay}")
-            print(f"self.cDeathDay {self.cDeathDay}")
+            if self.verbose:
+                print(f"\nTerminate: self.time_index is at or exceeds {self.nt - 1}\n")
+                print(f"self.simulationDays {self.simulationDays}")
+                print(f"self.growthDays {self.growthDays}")
+                print(f"self.therapyDays {self.therapyDays}")
+                print(f"self.postTherapyDays {self.postTherapyDays}")
+                print(f"self.firstTherapyDay {self.firstTherapyDay}")
+                print(f"self.firstPostTherapyDay {self.firstPostTherapyDay}")
+                print(f"self.cDeathDay {self.cDeathDay}")
             return True
         else:
             return False
@@ -239,14 +268,15 @@ class BrainTumor1D(PDEEnv1D):
                 if (self.stage == "Post-Therapy"):
                     self.postTherapyDays = self.time_index - self.therapyDays - self.growthDays
                     self.simulationDays = self.growthDays + self.therapyDays + self.postTherapyDays
-                print(f"\nTruncate: Tumor T1TumorRadius {T1TumorRadius} is at or exceeds self.t1_death_radius {self.t1_death_radius}mm\n")
-                print(f"self.simulationDays {self.simulationDays}")
-                print(f"self.growthDays {self.growthDays}")
-                print(f"self.therapyDays {self.therapyDays}")
-                print(f"self.postTherapyDays {self.postTherapyDays}")
-                print(f"self.firstTherapyDay {self.firstTherapyDay}")
-                print(f"self.firstPostTherapyDay {self.firstPostTherapyDay}")
-                print(f"self.cDeathDay {self.cDeathDay}")
+                if self.verbose:
+                    print(f"\nTruncate: Tumor T1TumorRadius {T1TumorRadius} is at or exceeds self.t1_death_radius {self.t1_death_radius}mm\n")
+                    print(f"self.simulationDays {self.simulationDays}")
+                    print(f"self.growthDays {self.growthDays}")
+                    print(f"self.therapyDays {self.therapyDays}")
+                    print(f"self.postTherapyDays {self.postTherapyDays}")
+                    print(f"self.firstTherapyDay {self.firstTherapyDay}")
+                    print(f"self.firstPostTherapyDay {self.firstPostTherapyDay}")
+                    print(f"self.cDeathDay {self.cDeathDay}")
                 return True
         return False
         
@@ -282,43 +312,52 @@ class BrainTumor1D(PDEEnv1D):
         )
 
 class TherapyWrapper(gym.Wrapper):
-    def __init__(self, env: BrainTumor1D):
+    def __init__(self, env: BrainTumor1D, verbose=True):
         super().__init__(env)
+        self.verbose = verbose
     
     # Run growth stage of simulation until start of therapy
-    def reset(self):
-        print(f"Wrapper: Reset environment")
+    def reset(self, seed: Optional[int]=None, options: Optional[dict]=None):
+        if self.verbose:
+            print(f"Wrapper: Reset environment")
         obs, info = self.env.reset()
 
-        print(f"Wrapper: Start Growth Stage")
+        if self.verbose:
+            print(f"Wrapper: Start Growth Stage")
         while self.env.unwrapped.stage == "Growth":
             obs, _, terminated, truncated, info = self.env.step(0)
             if terminated or truncated:
                 break
-        print(f"Wrapper: End Growth Stage\n")
+        if self.verbose:
+            print(f"Wrapper: End Growth Stage\n")
         return obs, info
     
     # Returns therapy reward if normal therapy step. Otherwise return episode reward
     def step(self, control: float):
-        print(f"Wrapper: Therapy step()")
+        if self.verbose:
+            print(f"Wrapper: Therapy step()")
         obs, reward, terminated, truncated, info = self.env.step(control)
 
         # Internally simulate Post-Therapy until terminate or truncate
         if not (terminated or truncated) and self.env.unwrapped.stage == "Post-Therapy":
-            print(f"Wrapper: Post-Therapy step()")
+            if self.verbose:
+                print(f"Wrapper: Post-Therapy step()")
             while not (terminated or truncated):
                 obs, reward, terminated, truncated, info = self.env.step(0)
         
         if (terminated or truncated):
-            print(f"[Episode Reward] {reward}\n")
+            if self.verbose:
+                print(f"[Episode Reward] {reward}\n")
         else:
-            print(f"[Therapy Reward] {reward}\n")
+            if self.verbose:
+                print(f"[Therapy Reward] {reward}\n")
 
         return obs, reward, terminated, truncated, info
     
     def benchmark(self):
         obs, info = self.env.reset()
-        print(f"Wrapper: Benchmark (episode run with no action and no reward):")
+        if self.verbose:
+            print(f"Wrapper: Benchmark (episode run with no action and no reward):")
         
         terminated = False
         truncated = False
@@ -326,10 +365,11 @@ class TherapyWrapper(gym.Wrapper):
         while not (terminated or truncated):
             obs, _, terminated, truncated, info = self.env.step(0)
 
-        # reset environment
+        # set t_benchmark and reset environment
         t_benchmark = self.env.unwrapped.simulationDays
         self.env.unwrapped.t_benchmark = t_benchmark
-        print(f"Set t_benchmark = {t_benchmark}")
+        if self.verbose:
+            print(f"Set t_benchmark = {t_benchmark}")
         obs, info = self.env.reset()
 
         return t_benchmark
--- pde_control_gym/src/rewards/brain_tumor_reward.py
+++ pde_control_gym/src/rewards/brain_tumor_reward.py
@@ -4,15 +4,17 @@ from typing import Optional
 
 class BrainTumorReward(BaseReward):
     
-    def reward(self, uVec: np.ndarray =None, time_index: int = None, terminate: Optional[bool] =None, truncate: Optional[bool] =None, action: Optional[float] =None, **kwargs):
+    def reward(self, uVec: np.ndarray =None, time_index: int = None, terminate: Optional[bool] =None, truncate: Optional[bool] =None, action: Optional[float] =None, verbose = True, **kwargs):
         # Episode reward
         t_benchmark = kwargs["t_benchmark"]
         if t_benchmark is None:
-            print(f"Warning: t_benchmark is not yet set -> returned reward of 0\n")
+            if verbose:
+                print(f"Warning: t_benchmark is not yet set -> returned reward of 0\n")
             return 0
 
         if (terminate or truncate):
-            print(f"Reward Class: time_index - t_benchmark = {time_index} - {t_benchmark}")
+            if verbose:
+                print(f"Reward Class: time_index - t_benchmark = {time_index} - {t_benchmark}")
             return time_index - t_benchmark
           
         # Treatment reward
@@ -23,15 +25,16 @@ class BrainTumorReward(BaseReward):
         treatment_radius = kwargs["treatment_radius"]
         applied_dosage = kwargs["applied_dosage"]
 
-        r_control = 1 / (1 + tumor_radius)
-        r_toxic = 0.1 * (max(0, (applied_dosage / dmaxsafe(treatment_radius)) - 1) ** 2)
+        lambda_control = 1
+        lambda_toxic = 0.2
+        if (tumor_radius is None):
+            r_control = 1
+        else:
+            r_control = 1 / (1 + tumor_radius)
+        r_toxic = max(0, (applied_dosage / dmaxsafe(treatment_radius)) - 1) ** 2
 
-        print(f"Reward Class: r_control - r_toxic = {r_control} - {r_toxic}")
-        print(f"\tParams: tumor_radius={tumor_radius} treatment_radius={treatment_radius} applied_dosage={applied_dosage} dmaxsafe(treatment_radius)={dmaxsafe(treatment_radius)}")
-        return r_control - r_toxic
-    '''
-
-    def reward():
-        return 0
-    '''  
+        if verbose:
+            print(f"Reward Class: l_c*r_control - l_t*r_toxic = {lambda_control * r_control} - {lambda_toxic * r_toxic}")
+            print(f"\tParams: tumor_radius={tumor_radius} treatment_radius={treatment_radius} applied_dosage={applied_dosage} dmaxsafe(treatment_radius)={dmaxsafe(treatment_radius)}")
+        return lambda_control * r_control - lambda_toxic * r_toxic
 
